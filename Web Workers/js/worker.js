d422acaad38c827a86af432f12ccc9e3f5094897
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-02-05 00:30:24 -0500

    web workers

---
+++ Web Workers/js/worker.js
@@ -0,0 +1,23 @@
+// Un worker no tiene acceso al DOM
+// Solo se comunica atrave de mensajes
+
+self.addEventListener('message', (ev) => {
+  self.postMessage(searchPrimes(parseInt(ev.data)));
+}, false)
+
+function searchPrimes(n) {
+  if (n < 2) return [];
+  console.log(n);
+  const primes = [2];
+  for (let i = 3; i <= n; i++) {
+    let flag = false;
+    for (let j = 0; j < primes.length; j++) {
+      if (i % primes[j] === 0) {
+        flag = true;
+        break;
+      }
+    }
+    if (!flag) primes.push(i);
+  }
+  return primes;
+}
